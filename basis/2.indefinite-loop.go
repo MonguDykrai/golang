efc1354334352538a8f88c490473d1b9c664217b
jicaifeng <jicaifeng@supremind.com>
2022-10-09 09:46:22 +0800

    Third Commit

---
+++ basis/2.indefinite-loop.go
@@ -0,0 +1,30 @@
+// https://www.codecademy.com/learn/learn-go-loops-arrays-maps-and-structs/modules/learn-go-loops/cheatsheet
+// https://www.geeksforgeeks.org/fmt-scan-function-in-golang-with-examples/
+package main
+
+import (
+	"fmt"
+)
+
+func ask() int {
+	var input int
+	fmt.Print("I am thinking of a number between 1-100: ")
+	// Get the input from the user
+	fmt.Scan(&input)
+	return input
+}
+
+func main() {
+	var guess int
+
+	// WRITE THE INDEFINITE LOOP CONTAINING THE ASK FUNCTION BELOW
+	guess = ask()
+
+	for guess != 56 {
+		guess = ask()
+	}
+
+	if guess == 56 {
+		fmt.Println("You are correct! You may go through to the treasure!")
+	}
+}
